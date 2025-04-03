355af2ae227b3f956c6318bed13dc21668928388
bpdtechlead <bpdtechlead@gmail.com>
2025-04-03 13:29:03 +0530

    2nd commit

--- one.php
+++ one.php
@@ -7,4 +7,7 @@ echo "Hello TestOne";
 git config --global user.email "bpdtechlead@gmail.com"
   git config --global user.name "bpdtechlead"
 
+  git remote -v
+
+  git remote add origin https://github.com/OWNER/REPOSITORY.git
 */
